7c2e4c80b6b462d99b131108c66c20bc303bff71
hbenazza <hbenazza@e2r8p1.1337.ma>
2025-07-13 20:15:49 +0100

    added headers folder

---
+++ headers/webserver.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   webserver.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbenazza <hbenazza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/30 20:33:58 by hbenazza          #+#    #+#             */
+/*   Updated: 2025/06/30 20:37:35 by hbenazza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEBSERVER_HPP
+#define WEBSERVER_HPP
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+
+
+#endif
