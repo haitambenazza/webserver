b277b88171704d1fab6e7969becafa5e4af6a699
hbenazza <hbenazza@c4r10p1.1337.ma>
2025-07-18 20:14:18 +0100

    when the NON-blocking I/O is enable the msg doesnt recv to the serv we need to implement multiplexer...and without NON-blocking I/O the server recv garbage values

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: hbenazza <hbenazza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 14:16:20 by hbenazza          #+#    #+#             */
-/*   Updated: 2025/07/18 18:35:22 by hbenazza         ###   ########.fr       */
+/*   Updated: 2025/07/18 20:10:56 by hbenazza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -158,7 +158,7 @@ bool Check_if_valid(const std::vector<std::string> str)
 int main( int ac, char **av, char **envp )
 {
 	std::vector<Server> 		srvs;
-	char	buffer;
+	char	buffer[10];
 	int read;
 
 	(void)envp;
@@ -169,18 +169,16 @@ int main( int ac, char **av, char **envp )
 		return (1);
 	}
 	srvs = GetFullServers( av[1] );
-	for (int i = 0; i < (int)srvs.size(); i++)
-
 	while (true)
 	{
 		for (int i = 0; i < (int)srvs.size(); i++)
 		{
 			srvs[i].Setfd_endpoint(accept(srvs[i].Getfd(), NULL, NULL));
-			fcntl(srvs[i].Getfd_endpoint(), F_SETFL, O_NONBLOCK);
 			if (srvs[i].Getfd_endpoint() != -1)
 			{
+				// fcntl(srvs[i].Getfd_endpoint(), F_SETFL, O_NONBLOCK);
 				std::cout << "a new client is connected to " << srvs[i].Getfd_endpoint() << '\n';
-				while ( (read = recv(srvs[i].Getfd_endpoint(), &buffer, 1, 0)) > 0)
+				while ( (read = recv(srvs[i].Getfd_endpoint(), &buffer, 10, 0)) > 0)
 					std::cout << buffer <<read  <<'\n';
 				srvs[i].CloseFd();
 			}
