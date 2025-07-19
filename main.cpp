86f2a0fb477b2249ba72007293092620f65f2585
kbassim <kbassim@c4r10p9.1337.ma>
2025-07-19 02:06:07 +0100

    merge conflict resolved

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: kbassim <kbassim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 14:16:20 by hbenazza          #+#    #+#             */
-/*   Updated: 2025/07/19 01:51:06 by kbassim          ###   ########.fr       */
+/*   Updated: 2025/07/19 02:05:14 by kbassim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -163,6 +163,7 @@ int main( int ac, char **av, char **envp )
 	std::string buffer;
 	char tmp[100] = {0};
 
+
 	(void)envp;
 	if (ac != 2)
 	{
@@ -186,7 +187,7 @@ int main( int ac, char **av, char **envp )
 			{
 				fcntl(srvs[i].Getfd_endpoint(), F_SETFL, O_NONBLOCK);
 				std::cout << "a new client is connected to " << srvs[i].Getfd_endpoint() << '\n';
-				while ( ( recv(srvs[i].Getfd_endpoint(), &tmp, 10, 0)) > 0)
+				while ( (recv(srvs[i].Getfd_endpoint(), &tmp, 10, 0)) > 0)
 					buffer += tmp;
 				std::cout << buffer << '\n';
 				buffer.clear();
