1de2b664c32aa8748746ab9d754819a53c95be0f
hbenazza <hbenazza@c4r10p8.1337.ma>
2025-07-24 02:51:18 +0100

    patching...

--- Includes/Macros.hpp
+++ Includes/Macros.hpp
@@ -6,11 +6,16 @@
 /*   By: hbenazza <hbenazza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 21:53:32 by hbenazza          #+#    #+#             */
-/*   Updated: 2025/07/14 22:08:26 by hbenazza         ###   ########.fr       */
+/*   Updated: 2025/07/24 02:24:13 by hbenazza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #define MAX_CLIENT_BODY 2147483647
+#define PORT "8080"
+#define IP "0.0.0.0"
+#define ROOT "/"
+#define SERVER_NAME ""
+#define INDEX "/index.html"
 
