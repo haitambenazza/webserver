1cbd4a2576da1e30ac20b9b8b2b531828769a917
hbenazza <hbenazza@c4r10p6.1337.ma>
2025-07-27 04:37:02 +0100

    new bug in BUGS.txt.. need to implement timeout

--- Includes/Server.hpp
+++ Includes/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: hbenazza <hbenazza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/12 03:12:13 by kbassim           #+#    #+#             */
-/*   Updated: 2025/07/26 17:39:04 by hbenazza         ###   ########.fr       */
+/*   Updated: 2025/07/27 03:21:39 by hbenazza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ class Server
         std::map < std::string, std::vector< std::string > >    Commands;
         std::vector < Location >                                Locations;
         int16_t	fd;
+
 		std::string	server_name;
 		std::string	port;
 		std::string	ip;
@@ -30,6 +31,7 @@ class Server
 		std::string index;
 		u_int64_t	max_body_size;
 		std::map<u_int16_t , std::string>error_map;
+
     public :
         Server();
         Server( const Server& copy );
