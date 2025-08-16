2d17f21663f64e45244219a752ab280eee01f4b4
haitambenazza <haitambenazza21@gmail.com>
2025-08-16 21:52:47 +0100

    fetching POST

--- Includes/Macros.hpp
+++ Includes/Macros.hpp
@@ -12,4 +12,4 @@
 #define ALLOWED_CHAR_URI "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ._~:/?#[]@!$&'()*+,;=%"
 #define TIMEOUT_CLIENT 10
 #define URI_MAX_LENGTH 4096
-#define BUFFER_SIZE 10024
+#define BUFFER_SIZE 4096
