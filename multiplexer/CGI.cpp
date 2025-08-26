214a4cd715e2df706b867c65f861615c4ffbe44b
govos <moubine41@gmail.com>
2025-08-26 03:42:26 +0100

    still working on cgi

--- multiplexer/CGI.cpp
+++ multiplexer/CGI.cpp
@@ -12,10 +12,14 @@ void    Cgi::SetEnv( Request& Req )
     env.push_back("SCRIPT_NAME=" + Req.GetScriptName());
     env.push_back("SCRIPT_FILENAME=" + Req.GetScriptPath());
     env.push_back("QUERY_STRING=" + Req.getQueryString());
+    env.push_back("CONTENT_TYPE=" + Req.getHeaderValue("Content-Type"));
+    env.push_back("CONTENT_LENGTH=" + Req.getHeaderValue("Content-Length"));
+
     env.push_back("GATEWAY_INTERFACE=CGI/1.1");
     env.push_back("SERVER_PROTOCOL=HTTP/1.1");
     // env.push_back("SERVER_SOFTWARE=YourServer/1.0");
     env.push_back("REQUEST_URI=" + Req.getUri());
+    ExecuteCgi(Req);
 }
 
 std::vector<char *> Cgi::GetEnvCgi()
@@ -34,18 +38,21 @@ void    Cgi::ExecuteCgi(Request &req)
 {
     if (pipe(fd) < 0)
     {
+        std::cerr << "Pipe creation failed" << std::endl;
         return;
     }
     
     child_pid = fork();
     if (child_pid == -1)
     {
+        std::cerr << "Fork failed" << std::endl;
         close(fd[0]);
         close(fd[1]);
         return;
     } 
     else if (child_pid == 0)
     {
+        std::cout << "aaaaaaaaaaaah\n";
         close(fd[0]);
         dup2(fd[1] , STDOUT_FILENO);
         close(fd[1]);
@@ -56,14 +63,14 @@ void    Cgi::ExecuteCgi(Request &req)
         cmds[0] = (char *)req.GetScriptPath().c_str();
         cmds[1] = (char *)req.GetScriptName().c_str();
         cmds[2] = NULL;
-
-        execve(cmds[0] , cmds , envVars.data());
+        if (execve(cmds[0] , cmds , envVars.data()) == -1)
+            perror("execve");
         exit(1);
     }
     else
     {
         close(fd[1]);
-
+        std::cout << "aaaa00aah\n";
         char buffer[4096];
         ssize_t bytes_read;
         output.clear();
@@ -78,5 +85,10 @@ void    Cgi::ExecuteCgi(Request &req)
         int status;
 
         waitpid(child_pid , &status , 0);
+
+        if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
+            std::cerr << "CGI script execution failed" << std::endl;
+            output = "Content-Type: text/html\r\n\r\n<h1>500 Internal Server Error</h1>";
+        }
     }
 }
