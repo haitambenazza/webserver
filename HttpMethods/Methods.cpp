ca52865605761c04a1c831cf7273b7bad818d061
govos <moubine41@gmail.com>
2025-09-09 01:38:55 +0100

    mergin with khalil to test smtg

--- HttpMethods/Methods.cpp
+++ HttpMethods/Methods.cpp
@@ -209,7 +209,7 @@ bool SendCgiData(Server& s, Multiplexer& m, int i, Cgi& cgi)
     size_t toSend = m.GetClient()[i].GetFileSize() - m.GetClient()[i].GetSentSize();
     if (toSend > 0)
     {
-        ssize_t sent = send(m.GetEvents()[i].data.fd, response.str().c_str() + m.GetClient()[i].GetSentSize(), toSend, MSG_NOSIGNAL);
+        ssize_t sent = send(m.GetEvents()[i].data.fd, response.str().c_str() + m.GetClient()[i].GetSentSize(), toSend, 0);
         if (sent >= 0)
             m.GetClient()[i].SetSentSize((size_t) sent);
     }
@@ -251,7 +251,7 @@ bool SendData( Server&s ,Multiplexer &m, int i, int status, std::string FullPath
 	size_t toSend =  m.GetClient()[i].GetFileSize() - m.GetClient()[i].GetSentSize();
 	if (toSend > 0)
 	{
-		ssize_t sent = send(m.GetEvents()[i].data.fd, response.str().c_str() + m.GetClient()[i].GetSentSize(), toSend , MSG_NOSIGNAL);
+		ssize_t sent = send(m.GetEvents()[i].data.fd, response.str().c_str() + m.GetClient()[i].GetSentSize(), toSend , 0);
 		if (sent >= 0)
 			m.GetClient()[i].SetSentSize((size_t) sent);
 	}
